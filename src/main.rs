7ed4274383ef02b93d199febeea65436a2a768f4
Geoffroy Lesage <gefthefrench@gmail.com>
2023-04-19 11:28:49 +1000

    Basic working surrealDB connection with services model

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,49 @@
-use actix_web::{get, App, Result, HttpResponse, HttpServer};
+use actix_web::{App, get, HttpResponse, HttpServer, Result};
+use surrealdb::engine::remote::ws::Client;
+use surrealdb::engine::remote::ws::Ws;
+use surrealdb::opt::auth::Root;
+use surrealdb::Surreal;
 
+mod error;
+mod service;
+
+static DB: Surreal<Client> = Surreal::init();
+
+/**
+ * Endpoints
+ **/
 #[get("/health")]
 pub async fn health() -> Result<HttpResponse> {
     Ok(HttpResponse::Ok().body("success".to_string()))
 }
 
+/**
+ * Main
+ **/
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(health))
-    .bind(("0.0.0.0", 8080))?
-    .run()
-    .await
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+	DB.connect::<Ws>("localhost:8000").await?;
+
+	DB.signin(Root {
+		username: "root",
+		password: "root",
+	})
+	.await?;
+
+	DB.use_ns("namespace").use_db("database").await?;
+
+	HttpServer::new(|| {
+		App::new()
+            .service(health)
+			.service(service::create)
+			.service(service::read)
+			.service(service::update)
+			.service(service::delete)
+			.service(service::list)
+	})
+	.bind(("0.0.0.0", 8080))?
+	.run()
+	.await?;
+
+	Ok(())
 }
---
+++ src/service.rs
@@ -0,0 +1,45 @@
+use crate::error::Error;
+use crate::DB;
+use actix_web::web::Json;
+use actix_web::web::Path;
+use actix_web::{delete, get, post, put};
+use serde::Deserialize;
+use serde::Serialize;
+
+const SERVICE: &str = "service";
+
+#[derive(Serialize, Deserialize)]
+pub struct Service {
+	name: String,
+	description: String
+}
+
+#[post("/service/{id}")]
+pub async fn create(id: Path<String>, service: Json<Service>) -> Result<Json<Service>, Error> {
+	let service = DB.create((SERVICE, &*id)).content(service).await?;
+	Ok(Json(service))
+}
+
+#[get("/service/{id}")]
+pub async fn read(id: Path<String>) -> Result<Json<Option<Service>>, Error> {
+	let service = DB.select((SERVICE, &*id)).await?;
+	Ok(Json(service))
+}
+
+#[put("/service/{id}")]
+pub async fn update(id: Path<String>, service: Json<Service>) -> Result<Json<Service>, Error> {
+	let service = DB.update((SERVICE, &*id)).content(service).await?;
+	Ok(Json(service))
+}
+
+#[delete("/service/{id}")]
+pub async fn delete(id: Path<String>) -> Result<Json<Option<Service>>, Error> {
+	let service = DB.delete((SERVICE, &*id)).await?;
+	Ok(Json(service))
+}
+
+#[get("/services")]
+pub async fn list() -> Result<Json<Vec<Service>>, Error> {
+	let services = DB.select(SERVICE).await?;
+	Ok(Json(services))
+}
