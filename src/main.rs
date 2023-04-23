c16ecf37f067acd1a43800ab3443d9cc2dccc1d8
Geoffroy Lesage <gefthefrench@gmail.com>
2023-04-23 18:38:31 +1000

    Added basic websocket handlers

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Result};
+use actix::{Actor, StreamHandler};
+use actix_web::{get, web, App, Error, HttpRequest, HttpResponse, HttpServer, Result};
+use actix_web_actors::ws;
 use configuration::read_env_vars;
 use configuration::Configuration;
+use serde::Deserialize;
 use surrealdb::engine::remote::ws::Client;
 use surrealdb::engine::remote::ws::Ws;
 use surrealdb::opt::auth::Root;
@@ -10,6 +13,18 @@ mod configuration;
 mod error;
 mod service;
 
+#[derive(Debug, Deserialize)]
+pub struct WSParams {
+    token: String,
+}
+
+/// Define HTTP actor
+struct MyWs;
+
+impl Actor for MyWs {
+    type Context = ws::WebsocketContext<Self>;
+}
+
 static DB: Surreal<Client> = Surreal::init();
 
 /**
@@ -18,6 +33,31 @@ static DB: Surreal<Client> = Surreal::init();
 #[get("/health")]
 pub async fn health() -> Result<HttpResponse> {
     Ok(HttpResponse::Ok().body("success".to_string()))
+}
+
+/// Handler for ws::Message message
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
+    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+        match msg {
+            Ok(ws::Message::Text(text)) => {
+                println!("received {:?}", text);
+                ctx.text(text)
+            }
+            _ => (),
+        }
+    }
+}
+
+async fn index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+    let params =
+        web::Query::<WSParams>::from_query(req.query_string()).unwrap_or(web::Query(WSParams {
+            token: String::from("token"),
+        }));
+    let token = &params.token;
+    println!("req token: {:?}", token);
+    // Here we need to validate the token with the DB. If it is not recognized, fail
+
+    ws::start(MyWs {}, &req, stream)
 }
 
 /**
@@ -42,6 +82,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     HttpServer::new(|| {
         App::new()
+            .service(web::resource("/ws").to(index))
             .service(health)
             .service(service::create)
             .service(service::read)
