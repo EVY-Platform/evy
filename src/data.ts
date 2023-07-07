41d39bf3c5083cbfd470bd33cac5538cfe7a8f86
Geoffroy Lesage <gefthefrench@gmail.com>
2023-07-07 23:25:11 +1000

    Moved database helpers to data

---
+++ src/data.ts
@@ -0,0 +1,44 @@
+import { v4 as uuidv4 } from 'uuid';
+import { PrismaClient, Device } from '@prisma/client';
+
+const prisma = new PrismaClient();
+const deviceTokens: string[] = [];
+
+const validOSes: string[] = ['ios', 'android'];
+
+async function primeDevicesCache(): Promise<void>
+{
+    const dbDevices = await prisma.device.findMany();
+    deviceTokens.push(...dbDevices.map(d => d.token));
+
+    console.info(`Primed cache with ${deviceTokens.length} device tokens`);
+}
+async function registerDevice(token: string, os: string): Promise<void>
+{
+    const newDevice: Device = await prisma.device.create({
+        data: {
+            id: uuidv4(),
+            token,
+            os,
+            created_at: new Date(),
+            updated_at: new Date()
+        }
+    });
+    deviceTokens.push(token);
+}
+export function validateAuth(token: string, os: string): boolean
+{
+    if (!token || token.length < 1) return false;
+    if (!os || !validOSes.includes(os)) return false;
+
+    if (!deviceTokens.includes(token))
+    {
+        // Yes this is odd, but rpc-json isn't async
+        // so we just have to trigger this and hope it works
+        registerDevice(token, os);
+    }
+
+    return true;
+}
+
+primeDevicesCache();
--- src/index.ts
+++ src/index.ts
@@ -1,67 +1,21 @@
 import dotenv from 'dotenv';
 dotenv.config();
-import { v4 as uuidv4 } from 'uuid';
-import { PrismaClient, Device } from '@prisma/client';
-import { initServer, IRPCMethodParams } from './ws/ws.js';
+
+import { validateAuth } from './data.js';
+import { initServer, IRPCMethodParams } from './ws.js';
 
 const HOST: string | undefined = process.env.API_HOST;
 const PORT: string | undefined = process.env.API_PORT;
 
-const prisma = new PrismaClient();
-const deviceTokens: string[] = [];
-
-const validOSes: string[] = ['ios', 'android'];
-
-async function primeDevicesCache(): Promise<void>
-{
-    const dbDevices = await prisma.device.findMany();
-    deviceTokens.push(...dbDevices.map(d => d.token));
-
-    console.info(`Primed cache with ${deviceTokens.length} device tokens`);
-}
-async function registerDevice(token: string, os: string): Promise<void>
-{
-    const newDevice: Device = await prisma.device.create({
-        data: {
-            id: uuidv4(),
-            token,
-            os,
-            created_at: new Date(),
-            updated_at: new Date()
-        }
-    });
-    deviceTokens.push(token);
-}
 function authHandler(params: IRPCMethodParams): boolean
 {
-    if (!params.token || params.token.length < 1) return false;
-    if (!params.os || !validOSes.includes(params.os)) return false;
-
-    if (!deviceTokens.includes(params.token))
-    {
-        // Yes this is odd, but rpc-json isn't async
-        // so we just have to trigger this and hope it works
-        registerDevice(params.token, params.os);
-    }
-
-    return true;
+    return validateAuth(params.token, params.os);
 }
 
 async function main()
 {
     if (!HOST || !PORT) return Promise.reject(new Error('Missing API environment variables'));
-    await primeDevicesCache();
     await initServer({ host: HOST, port: Number(PORT), namespace: 'admin', authHandler });
 }
 
-main()
-    .then(async () =>
-    {
-        await prisma.$disconnect();
-    })
-    .catch(async (e) =>
-    {
-        console.error(e);
-        await prisma.$disconnect();
-        process.exit(1);
-    });
+main();
