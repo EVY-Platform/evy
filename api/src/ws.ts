1fa8ecd1b2a3c263e93d30fae14b862bba3d7f3e
Geoffroy Lesage <gefthefrench@gmail.com>
2023-08-10 16:28:25 +1000

    Added traefik and letsencrypt configs

--- api/src/ws.ts
+++ api/src/ws.ts
@@ -4,23 +4,31 @@ type WSServer = typeof Server;
 type WSError = typeof IRPCError;
 export type WSParams = typeof IRPCMethodParams;
 
-const HOST: string | undefined = process.env.API_HOST;
-const PORT: string | undefined = process.env.API_PORT;
-if (!HOST || !PORT) throw new Error('Missing API environment variables');
+if (!process.env.API_PORT) throw new Error('Missing API_PORT environment variable');
+const PORT: number = parseInt(process.env.API_PORT);
+const HOST: string = "0.0.0.0";
 
 function initServer(authHandler: (params: WSParams) => boolean): Promise<WSServer>
 {
     return new Promise<WSServer>((resolve, reject) =>
     {
-        const server = new Server({ host: HOST, port: Number(PORT) });
+        const server = new Server({ host: HOST, port: PORT });
 
         server.on("listening", () => resolve(server));
         server.on("error", (error: WSError) => reject(error));
+        server.on("connection", () => {
+            console.log('connection')
+        });
     }).then(server =>
     {
+        server.register('sum', function() {
+            console.log('in sum')
+            return "Ok"
+        })
+
         server.setAuth(authHandler);
 
-        console.info(`WS server listening on ${HOST}:${PORT}`);
+        console.info(`WS server listening at ${HOST}:${PORT}`);
 
         return server;
     });
