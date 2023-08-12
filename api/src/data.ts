6c8c962f7b71bd896472e7c0b66ad38635aa6623
Geoffroy Lesage <gefthefrench@gmail.com>
2023-08-12 15:14:29 +1000

    Clean up usage of auth and enums

--- api/src/data.ts
+++ api/src/data.ts
@@ -1,9 +1,8 @@
 import { PrismaClient } from '@prisma/client';
-import type { Device, OS } from '@prisma/client';
+import type { Device } from '@prisma/client';
+import { OS } from '@prisma/client';
 
 const prisma = new PrismaClient();
-export const activeDeviceTokens: string[] = [];
-
 const validOSes: string[] = ['ios', 'android'];
 
 async function findOrCreateDevice(token: string, os: OS): Promise<Device|null>
@@ -25,11 +24,7 @@ async function findOrCreateDevice(token: string, os: OS): Promise<Device|null>
 export async function validateAuth(token: string, os: OS): Promise<boolean>
 {
     if (!token || token.length < 1) return false;
-    if (!os || !validOSes.includes(os)) return false;
+    if (!Object.values(OS).includes(os)) return false;
 
-    const device = await findOrCreateDevice(token, os).catch(() => false);
-    if (!device) return false
-
-    activeDeviceTokens.push(token);
-    return true;
+    return !!await findOrCreateDevice(token, os).catch(() => false);
 }
