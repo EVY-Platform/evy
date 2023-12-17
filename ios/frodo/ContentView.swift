3beb444b06da7d9c77866e9b90b7cb535e1fc240
Geoffroy Lesage <glesage@users.noreply.github.com>
2023-12-17 13:12:31 +1100

    Move row height to row definition (#4)
    * Move row height to row definition
    
    * Move listRow index too

--- ios/frodo/ContentView.swift
+++ ios/frodo/ContentView.swift
@@ -12,14 +12,7 @@ struct ContentView: View {
     
     var body: some View {
         List(rows.indices, id: \.self) { index in
-            if index == 0 {
-                rows[index]
-                    .frame(height: 250)
-                    .listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
-            } else {
-                rows[index]
-            }
-            
+            rows[index]
         }
         .listStyle(PlainListStyle())
         .ignoresSafeArea()
--- ios/frodo/Rows/EVYCarouselRow.swift
+++ ios/frodo/Rows/EVYCarouselRow.swift
@@ -55,6 +55,7 @@ struct EVYCarouselRow: View {
                          content: {EVYCarouselOverlay(imageNames: imageNames,
                                                       selectedIndex: selectedImageIndex)})
         .listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
+        .frame(height: 250)
     }
 }
 
