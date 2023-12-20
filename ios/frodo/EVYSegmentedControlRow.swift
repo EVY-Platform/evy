ba6e2f9aa64c2ed5e0ec91a7709ad11024657951
Geoffroy Lesage <gefthefrench@gmail.com>
2023-12-20 22:02:49 +1100

    Trim example for segmented control row

--- ios/frodo/EVYSegmentedControlRow.swift
+++ ios/frodo/EVYSegmentedControlRow.swift
@@ -51,302 +51,32 @@ struct EVYSegmentedControlRow: View {
                 {
                     "title": "Pickup",
                     "children": [{
-                        "type": "TimeslotPicker",
+                        "type": "Title",
                         "content": {
-                            "icon": "pickup",
-                            "subtitle": "Meet at the pickup address",
-                            "details": "+ $5.50",
-                            "dates_with_timeslots": [
-                                {
-                                    "header": "Wed",
-                                    "date": "8 nov.",
-                                    "timeslots": [
-                                        {
-                                            "timeslot": "11:30",
-                                            "available": true
-                                        },
-                                        {
-                                            "timeslot": "12:00",
-                                            "available": true
-                                        }
-                                    ]
-                                },
-                                {
-                                    "header": "Thu",
-                                    "date": "9 nov.",
-                                    "timeslots": [
-                                        {
-                                            "timeslot": "10:30",
-                                            "available": false
-                                        },
-                                        {
-                                            "timeslot": "11:00",
-                                            "available": true
-                                        },
-                                        {
-                                            "timeslot": "12:00",
-                                            "available": true
-                                        }
-                                    ]
-                                },
-                                {
-                                    "header": "Fri",
-                                    "date": "10 nov.",
-                                    "timeslots": [
-                                        {
-                                            "timeslot": "10:30",
-                                            "available": true
-                                        },
-                                        {
-                                            "timeslot": "12:00",
-                                            "available": false
-                                        },
-                                        {
-                                            "timeslot": "12:30",
-                                            "available": true
-                                        }
-                                    ]
-                                },
-                                {
-                                    "header": "Sat",
-                                    "date": "11 nov.",
-                                    "timeslots": [
-                                        {
-                                            "timeslot": "11:30",
-                                            "available": true
-                                        },
-                                        {
-                                            "timeslot": "12:00",
-                                            "available": true
-                                        },
-                                        {
-                                            "timeslot": "13:00",
-                                            "available": true
-                                        }
-                                    ]
-                                },
-                                {
-                                    "header": "Sun",
-                                    "date": "12 nov.",
-                                    "timeslots": [
-                                        {
-                                            "timeslot": "10:30",
-                                            "available": true
-                                        },
-                                        {
-                                            "timeslot": "11:30",
-                                            "available": true
-                                        },
-                                        {
-                                            "timeslot": "12:00",
-                                            "available": true
-                                        }
-                                    ]
-                                },
-                                {
-                                    "header": "Sun",
-                                    "date": "13 nov.",
-                                    "timeslots": [
-                                        {
-                                            "timeslot": "10:30",
-                                            "available": true
-                                        },
-                                        {
-                                            "timeslot": "11:30",
-                                            "available": true
-                                        },
-                                        {
-                                            "timeslot": "12:00",
-                                            "available": true
-                                        }
-                                    ]
-                                },
-                                {
-                                    "header": "Sun",
-                                    "date": "14 nov.",
-                                    "timeslots": [
-                                        {
-                                            "timeslot": "10:30",
-                                            "available": true
-                                        },
-                                        {
-                                            "timeslot": "11:30",
-                                            "available": true
-                                        },
-                                        {
-                                            "timeslot": "12:00",
-                                            "available": true
-                                        }
-                                    ]
-                                },
-                                {
-                                    "header": "Sun",
-                                    "date": "15 nov.",
-                                    "timeslots": [
-                                        {
-                                            "timeslot": "10:30",
-                                            "available": true
-                                        },
-                                        {
-                                            "timeslot": "11:30",
-                                            "available": true
-                                        },
-                                        {
-                                            "timeslot": "12:00",
-                                            "available": true
-                                        }
-                                    ]
-                                }
-                            ]
+                            "title": "1",
+                            "title_detail": "$250",
+                            "subtitle_1": "::star.square.on.square.fill:: 88% - 4 items sold",
+                            "subtitle_2": "Rosebery, NSW  -  Posted on Nov 8th"
+                        }
+                    },{
+                        "type": "Title",
+                        "content": {
+                            "title": "2",
+                            "title_detail": "$250",
+                            "subtitle_1": "::star.square.on.square.fill:: 88% - 4 items sold",
+                            "subtitle_2": "Rosebery, NSW  -  Posted on Nov 8th"
                         }
                     }]
                 },
                 {
                     "title": "Deliver",
                     "children": [{
-                        "type": "TimeslotPicker",
+                        "type": "Title",
                         "content": {
-                            "icon": "delivery",
-                            "subtitle": "Delivered at your door",
-                            "details": "+ $10",
-                            "dates_with_timeslots": [
-                                {
-                                    "header": "Wed",
-                                    "date": "10 nov.",
-                                    "timeslots": [
-                                        {
-                                            "timeslot": "12:00",
-                                            "available": true
-                                        }
-                                    ]
-                                },
-                                {
-                                    "header": "Thu",
-                                    "date": "11 nov.",
-                                    "timeslots": [
-                                        {
-                                            "timeslot": "11:30",
-                                            "available": false
-                                        },
-                                        {
-                                            "timeslot": "12:00",
-                                            "available": true
-                                        },
-                                        {
-                                            "timeslot": "13:00",
-                                            "available": true
-                                        }
-                                    ]
-                                },
-                                {
-                                    "header": "Fri",
-                                    "date": "12 nov.",
-                                    "timeslots": [
-                                        {
-                                            "timeslot": "8:30",
-                                            "available": true
-                                        },
-                                        {
-                                            "timeslot": "10:00",
-                                            "available": false
-                                        },
-                                        {
-                                            "timeslot": "12:30",
-                                            "available": true
-                                        }
-                                    ]
-                                },
-                                {
-                                    "header": "Sat",
-                                    "date": "13 nov.",
-                                    "timeslots": [
-                                        {
-                                            "timeslot": "11:30",
-                                            "available": true
-                                        },
-                                        {
-                                            "timeslot": "12:00",
-                                            "available": true
-                                        },
-                                        {
-                                            "timeslot": "13:00",
-                                            "available": true
-                                        }
-                                    ]
-                                },
-                                {
-                                    "header": "Sun",
-                                    "date": "14 nov.",
-                                    "timeslots": [
-                                        {
-                                            "timeslot": "10:30",
-                                            "available": true
-                                        },
-                                        {
-                                            "timeslot": "11:30",
-                                            "available": true
-                                        },
-                                        {
-                                            "timeslot": "12:00",
-                                            "available": true
-                                        }
-                                    ]
-                                },
-                                {
-                                    "header": "Sun",
-                                    "date": "15 nov.",
-                                    "timeslots": [
-                                        {
-                                            "timeslot": "10:30",
-                                            "available": true
-                                        },
-                                        {
-                                            "timeslot": "11:30",
-                                            "available": true
-                                        },
-                                        {
-                                            "timeslot": "12:00",
-                                            "available": true
-                                        }
-                                    ]
-                                },
-                                {
-                                    "header": "Sun",
-                                    "date": "16 nov.",
-                                    "timeslots": [
-                                        {
-                                            "timeslot": "10:30",
-                                            "available": true
-                                        },
-                                        {
-                                            "timeslot": "11:30",
-                                            "available": true
-                                        },
-                                        {
-                                            "timeslot": "12:00",
-                                            "available": true
-                                        }
-                                    ]
-                                },
-                                {
-                                    "header": "Sun",
-                                    "date": "17 nov.",
-                                    "timeslots": [
-                                        {
-                                            "timeslot": "10:30",
-                                            "available": true
-                                        },
-                                        {
-                                            "timeslot": "11:30",
-                                            "available": true
-                                        },
-                                        {
-                                            "timeslot": "12:00",
-                                            "available": true
-                                        }
-                                    ]
-                                }
-                            ]
+                            "title": "3",
+                            "title_detail": "$250",
+                            "subtitle_1": "::star.square.on.square.fill:: 88% - 4 items sold",
+                            "subtitle_2": "Rosebery, NSW  -  Posted on Nov 8th"
                         }
                     }]
                 }
