6c242304be3c5b44ac519d738807776f0b276e41
Geoffroy Lesage <gefthefrench@gmail.com>
2024-02-11 19:41:47 +1100

    Delete obsolete components

--- ios/evy/EVYRow.swift
+++ ios/evy/EVYRow.swift
@@ -21,35 +21,8 @@ struct EVYRow: View, Decodable {
         self.type = try container.decode(String.self, forKey: .type)
         
         switch self.type {
-        case EVYCarouselRow.JSONType:
-            self.content = try EVYCarouselRow(container: container)
-            
-        case EVYTitleRow.JSONType:
-            self.content = try EVYTitleRow(container: container)
-            
-        case EVYTextRow.JSONType:
-            self.content = try EVYTextRow(container: container)
-            
-        case EVYTimeslotPickerRow.JSONType:
-            self.content = try EVYTimeslotPickerRow(container: container)
-            
-        case EVYDetailRow.JSONType:
-            self.content = try EVYDetailRow(container: container)
-            
-        case EVYContainerListRow.JSONType:
-            self.content = try EVYContainerListRow(container: container)
-            
         case EVYSegmentedControlRow.JSONType:
             self.content = try EVYSegmentedControlRow(container: container)
-            
-        case EVYDisclaimerRow.JSONType:
-            self.content = try EVYDisclaimerRow(container: container)
-            
-        case EVYPaymentOptionsRow.JSONType:
-            self.content = try EVYPaymentOptionsRow(container: container)
-            
-        case EVYAddressRow.JSONType:
-            self.content = try EVYAddressRow(container: container)
             
         default:
             self.content = Spacer()
