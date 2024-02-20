800a7ff0bf79da0716f89ffd3f5b387be948dcf1
Clémence CHALOT <clemence.chalot@gmail.com>
2024-02-20 21:38:06 +1100

    Created open gallery functionality
    Need to:
    - change the number of photos selection to 10 max (now just 1 picture can be selected)
    - change the display when the pictures are selected so it's inline (now show underneath the photo placeholder)

--- ios/evy/Rows/EVYSelectPhotoRow.swift
+++ ios/evy/Rows/EVYSelectPhotoRow.swift
@@ -6,8 +6,8 @@
 //
 
 
-
 import SwiftUI
+import PhotosUI
 
 struct EVYSelectPhotoRowView: Decodable {
     let content: ContentData
@@ -20,48 +20,78 @@ struct EVYSelectPhotoRowView: Decodable {
         let photo_ids: String
     }
 }
-    
+
 struct EVYSelectPhotoRow: View {
     public static var JSONType = "SelectPhoto"
-    
     private let view: EVYSelectPhotoRowView
     
     init(container: KeyedDecodingContainer<RowCodingKeys>) throws {
-        self.view = try container.decode(EVYSelectPhotoRowView.self, forKey:.view)
+        self.view = try container.decode(EVYSelectPhotoRowView.self, forKey: .view)
     }
-        
+
+    @State private var selectedItem: PhotosPickerItem?
+    @State private var image: UIImage?
+
     var body: some View {
         VStack {
-            if (view.content.title.count > 0) {
+            if view.content.title.count > 0 {
                 EVYText(view.content.title)
                     .font(.titleFont)
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .padding(.bottom, Constants.textLinePadding)
-            };
-            VStack {
-                EVYText(view.content.icon)
-                    .font(.titleFont)
-                    .foregroundColor(Constants.placeholderColor)
-                EVYText(view.content.subtitle)
-                    .font(.titleFont)
-                    .foregroundColor(Constants.placeholderColor)
             }
-            .padding(.vertical, 80)
-            .frame(maxWidth: .infinity, alignment: .center)
-            .background(
-                RoundedRectangle(cornerRadius: Constants.smallCornerRadius)
-                    .strokeBorder(Constants.fieldBorderColor, lineWidth: Constants.borderWidth))
-            
+
+            VStack {
+                PhotosPicker(
+                    selection: $selectedItem,
+                    label: {
+                        VStack {
+                            EVYText(view.content.icon)
+                                .font(.titleFont)
+                                .foregroundColor(Constants.placeholderColor)
+                            EVYText(view.content.subtitle)
+                                .font(.titleFont)
+                                .foregroundColor(Constants.placeholderColor)
+                        }
+                        .padding(.vertical, 80)
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .background(
+                            RoundedRectangle(cornerRadius: Constants.smallCornerRadius)
+                                .strokeBorder(Constants.fieldBorderColor, lineWidth: Constants.borderWidth)
+                        )
+                    }
+                )
+                .onChange(of: selectedItem) {
+                    Task {
+                        do {
+                            if let data = try await selectedItem?.loadTransferable(type: Data.self) {
+                                image = UIImage(data: data)
+                            }
+                        } catch {
+                            print("Failed to load the image")
+                        }
+                    }
+                }
+
+                if let image = image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+
             EVYText(view.content.content)
                 .font(.detailFont)
                 .foregroundColor(Constants.placeholderColor)
                 .frame(maxWidth: .infinity, alignment: .leading)
         }
+        .padding()
     }
 }
 
-
-#Preview {
-    let json =  SDUIConstants.selectPhotoRow.data(using: .utf8)!
-    return try? JSONDecoder().decode(EVYRow.self, from: json)
+struct EVYSelectPhotoRow_Previews: PreviewProvider {
+    static var previews: some View {
+        let json = SDUIConstants.selectPhotoRow.data(using: .utf8)!
+        return try? JSONDecoder().decode(EVYRow.self, from: json)
+    }
 }
