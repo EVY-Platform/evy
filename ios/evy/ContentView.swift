1d3c3b43728ffb76a46003c68076c7656a3c8752
Geoffroy Lesage <gefthefrench@gmail.com>
2024-03-26 22:50:12 +1100

    Move root to EVYHome and share logic for routes

--- ios/evy/ContentView.swift
+++ ios/evy/ContentView.swift
@@ -32,51 +32,51 @@ struct ContentView: View {
         self.flows = try! JSONDecoder().decode([EVYFlow].self, from: jsonFlow)
     }
     
+    private func handleNavigationData(route: Route, flow: EVYFlow?) {
+//        if flow.id != route.flowId {
+//            // If the old flow was for creation, submit the draft
+//            if flow.type == .create {
+//                try! EVYDataManager.i.submit(key: flow.data)
+//            }
+//
+//            if route.flowId != "home" {
+//                // If the new flow is for creation, start a draft
+//                let newFlow = flows.first(where: {$0.id == route.flowId})!
+//                if newFlow.type == .create {
+//                    let item = DataConstants.item.data(using: .utf8)!
+//                    try! EVYDataManager.i.create(key: newFlow.data, data: item)
+//                }
+//            }
+//            // If we are in the same create flow, update the draft
+//        } else if flow.type == .create {
+//            let item = DataConstants.item.data(using: .utf8)!
+//            try! EVYDataManager.i.update(key: flow.data, data: item)
+//        }
+                                    
+        if route.flowId == "home" {
+            routes.removeAll()
+        }
+        else if let existing = routes.lastIndex(of: route) {
+            routes.removeSubrange(existing...)
+        }
+        else {
+            routes.append(route)
+        }
+    }
+    
     var body: some View {
         NavigationStack(path: $routes) {
-            VStack(spacing: 40) {
-                Button("View Item") {
-                    routes.append(Route(flowId: "view_item", pageId: "view"))
-                }
-                Button("Create Item") {
-                    routes.append(Route(flowId: "create_item", pageId: "step_1"))
+            EVYHome()
+                .environment(\.navigate) { route in
+                    handleNavigationData(route: route, flow: nil)
                 }
                 .navigationDestination(for: Route.self) { route in
                     let flow = flows.first(where: {$0.id == route.flowId})!
-                    
                     flow.getPageById(route.pageId)!
                         .environment(\.navigate) { route in
-//                            if flow.id != route.flowId {
-//                                // If the old flow was for creation, submit the draft
-//                                if flow.type == .create {
-//                                    try! EVYDataManager.i.submit(key: flow.data)
-//                                }
-//                                
-//                                if route.flowId != "home" {
-//                                    // If the new flow is for creation, start a draft
-//                                    let newFlow = flows.first(where: {$0.id == route.flowId})!
-//                                    if newFlow.type == .create {
-//                                        let item = DataConstants.item.data(using: .utf8)!
-//                                        try! EVYDataManager.i.create(key: newFlow.data, data: item)
-//                                    }
-//                                }
-//                            // If we are in the same create flow, update the draft
-//                            } else if flow.type == .create {
-//                                let item = DataConstants.item.data(using: .utf8)!
-//                                try! EVYDataManager.i.update(key: flow.data, data: item)
-//                            }
-                            if route.flowId == "home" {
-                                routes.removeAll()
-                            }
-                            else if let existing = routes.lastIndex(of: route) {
-                                routes.removeSubrange(existing...)
-                            }
-                            else {
-                                routes.append(route)
-                            }
+                            handleNavigationData(route: route, flow: flow)
                         }
                 }
-            }
         }
     }
 }
