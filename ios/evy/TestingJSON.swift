99d82e8a4a685654b07e277b397e7553d513c268
Geoffroy Lesage <gefthefrench@gmail.com>
2024-06-29 18:40:09 +1000

    Fix conditional for last button

--- ios/evy/TestingJSON.swift
+++ ios/evy/TestingJSON.swift
@@ -339,7 +339,7 @@ struct SDUIConstants {
             "edit": {},
             "action": {
                 "target": "navigate:create_item:step_4",
-                "condition": "{count(item.title) > 20}"
+                "condition": "{count(item.title) > 10}"
             }
         }
     """
