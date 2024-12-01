f9f6e8157ff969fb86e0b9d0a9597795daf7ec94
Geoffroy Lesage <gefthefrench@gmail.com>
2024-12-01 17:37:38 +1100

    built with fresh

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -1,18 +1,7 @@
-import type { Config } from "tailwindcss";
+import { type Config } from "tailwindcss";
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "{routes,islands,components}/**/*.{ts,tsx,js,jsx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
 } satisfies Config;
