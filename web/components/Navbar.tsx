f45c8eb0749f92cb00bd8d24aec804eeaf7cb2d9
Geoffroy Lesage <gefthefrench@gmail.com>
2024-12-05 21:27:31 +1100

    Remove irrelevant page

--- web/components/Navbar.tsx
+++ web/components/Navbar.tsx
@@ -2,11 +2,8 @@ import Logo from "./Logo.tsx";
 
 export default function Navbar() {
 	return (
-		<nav class="bg-gray-50 flex flex-wrap items-center justify-between p-4">
+		<nav class="bg-gray-100 p-4">
 			<Logo />
-			<a href="/about" class="navbar-solid-bg font-medium">
-				About
-			</a>
 		</nav>
 	);
 }
--- web/fresh.gen.ts
+++ web/fresh.gen.ts
@@ -4,7 +4,6 @@
 
 import * as $_404 from "./routes/_404.tsx";
 import * as $_app from "./routes/_app.tsx";
-import * as $about_index from "./routes/about/index.tsx";
 import * as $index from "./routes/index.tsx";
 
 import type { Manifest } from "$fresh/server.ts";
@@ -13,7 +12,6 @@ const manifest = {
   routes: {
     "./routes/_404.tsx": $_404,
     "./routes/_app.tsx": $_app,
-    "./routes/about/index.tsx": $about_index,
     "./routes/index.tsx": $index,
   },
   islands: {},
