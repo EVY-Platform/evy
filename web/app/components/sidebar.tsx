e91ff449b2dd6313e07d1de698b3f71697842a7b
Geoffroy Lesage <gefthefrench@gmail.com>
2025-03-16 17:48:34 +1100

    Remove xcss from sidebar

--- web/app/components/sidebar.tsx
+++ web/app/components/sidebar.tsx
@@ -1,31 +1,11 @@
 import { memo, useEffect, useRef } from "react";
 
-import { Box, Flex, Stack, xcss } from "@atlaskit/primitives";
 import { combine } from "@atlaskit/pragmatic-drag-and-drop/combine";
 import { dropTargetForElements } from "@atlaskit/pragmatic-drag-and-drop/element/adapter";
 import invariant from "tiny-invariant";
 
 import { Row, type RowData } from "./row.tsx";
 import { CancelOverlay } from "./cancel-overlay.tsx";
-
-const sidebarStyles = xcss({
-	position: "relative",
-	width: "100%",
-	backgroundColor: "elevation.surface.sunken",
-});
-
-const rowListStyles = xcss({
-	boxSizing: "border-box",
-	minHeight: "100%",
-	padding: "space.100",
-	gap: "space.100",
-});
-
-const overlayStyles = xcss({
-	position: "absolute",
-	width: "100%",
-	height: "100%",
-});
 
 export const Sidebar = memo(function Sidebar({
 	rowsData,
@@ -52,22 +32,18 @@ export const Sidebar = memo(function Sidebar({
 	}, []);
 
 	return (
-		<Flex xcss={sidebarStyles}>
-			<Stack ref={pageInnerRef}>
+		<div className="flex relative w-full bg-evy-light-gray">
+			<div className="flex flex-col" ref={pageInnerRef}>
 				<div className="p-4 text-xl font-bold text-center">Rows</div>
-				<Stack xcss={rowListStyles} space="space.100">
+				<div className="flex flex-col min-h-full p-2 gap-2">
 					{rowsData.map((rowData) => (
 						<Row key={rowData.rowId} rowId={rowData.rowId}>
 							{rowData.row}
 						</Row>
 					))}
-				</Stack>
-				{dragging && (
-					<Box xcss={overlayStyles}>
-						<CancelOverlay />
-					</Box>
-				)}
-			</Stack>
-		</Flex>
+				</div>
+				{dragging && <CancelOverlay />}
+			</div>
+		</div>
 	);
 });
