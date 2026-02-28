8c81db27dd9494bf132a62636b44a920115c9dfb
Geoffroy Lesage <gefthefrench@gmail.com>
2026-03-01 00:10:41 +1100

    feat/cleanup playwright configs

--- web/playwright.config.js
+++ web/playwright.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
-	testDir: "./tests",
 	timeout: 10000,
 	fullyParallel: true,
-	forbidOnly: !!process.env.CI,
-	retries: process.env.CI ? 2 : 1,
-	workers: process.env.CI ? 1 : undefined,
+	workers: 8,
 	reporter: "line",
 	use: {
 		baseURL: "http://localhost:3000",
@@ -24,6 +21,6 @@ export default defineConfig({
 	webServer: {
 		command: "bun run dev",
 		url: "http://localhost:3000",
-		reuseExistingServer: !process.env.CI,
+		reuseExistingServer: process.env.CI !== "true",
 	},
 });
