84e914183a2e392d0a25344950365e399b9c31d6
Geoffroy Lesage <gefthefrench@gmail.com>
2026-03-01 12:26:37 +1100

    Remove generated types

--- types/generated/ts/common/json.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-/** Generated from schema/common/json.schema.json - do not edit. */
-
-export interface CommonJSON {
-  [k: string]: string | number | boolean | null;
-}
--- types/generated/ts/common/rpc.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-/** Generated from schema/common/rpc.schema.json - do not edit. */
-
-/**
- * Shared RPC request/response definitions
- */
-export interface CommonRPC {
-  id: string;
-}
