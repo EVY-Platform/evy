395a66aa437143ff0f94185cacbce57b8c01dc9b
Geoffroy Lesage <gefthefrench@gmail.com>
2026-04-29 01:34:16 +1000

    update preview

--- ios/evy/UI/Views/EVYSearchSingle.swift
+++ ios/evy/UI/Views/EVYSearchSingle.swift
@@ -111,15 +111,38 @@ struct EVYSearchSingle: View {
 }
 
 #Preview {
-  AsyncPreview { asyncView in
+  AsyncPreview { (asyncView: EVYSearch) in
     asyncView
   } view: {
-    try! await EVY.createItem()
-
+    // Local-only: no EVY.getRow / EVYAPIManager (avoids API_HOST fatalError in Xcode canvas).
+    if !EVY.publicStore.exists(key: "tags") {
+      try EVY.publicStore.create(key: "tags", data: Data("[]".utf8))
+    }
+    let templateJson = """
+      {
+      	"id": "preview-search-row",
+      	"type": "Info",
+      	"source": "",
+      	"destination": "",
+      	"actions": [],
+      	"view": {
+      		"content": {
+      			"title": "{$datum:unit} {$datum:street}",
+      			"subtitle": "{$datum:city} {$datum:state} {$datum:postcode}",
+      			"icon": ""
+      		}
+      	}
+      }
+      """
+    let template = try JSONDecoder().decode(
+      UI_Row.self,
+      from: Data(templateJson.utf8),
+    )
     return EVYSearch(
       source: "{$local:address}",
-      destination: "{address}",
+      destination: "",
       placeholder: "Search",
-      resultTemplate: nil)
+      resultTemplate: template
+    )
   }
 }
